74db9b18bc954abbfdf3c29dc62bb87029a0c257
acs2405 <adrisiudad@gmail.com>
2022-11-07 12:05:58 +0000

    Deploying to gh-pages from  @ f5263311a39663691ee640d6d78de3d81a993279 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['codingrule_1',['codingRule',['../classlsysgen_1_1codingRule.html',1,'lsysgen::codingRule&lt; typename &gt;'],['../classlsysgen_1_1CodingRule.html',1,'lsysgen::CodingRule&lt; T &gt;']]],
+  ['codingrule_1',['CodingRule',['../classlsysgen_1_1CodingRule.html',1,'lsysgen']]],
   ['color_2',['Color',['../structColor.html',1,'']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['codingrule_53',['codingRule',['../classlsysgen_1_1codingRule.html',1,'lsysgen::codingRule&lt; typename &gt;'],['../classlsysgen_1_1CodingRule.html',1,'lsysgen::CodingRule&lt; T &gt;']]],
+  ['codingrule_53',['CodingRule',['../classlsysgen_1_1CodingRule.html',1,'lsysgen']]],
   ['color_54',['Color',['../structColor.html',1,'']]]
 ];
